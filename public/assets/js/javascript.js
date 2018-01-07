a330ea3c7bbe17c1e879b2411003194ccb0e8b7d
emfritz2 <emfritz2@gmail.com>
2018-01-07 16:00:50 -0600

    homepage created with loginand sign up modals

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -7,3 +7,16 @@ $(".button-collapse").sideNav();
 
  $('.dropdown-button').dropdown('open');
  $('.dropdown-button').dropdown('close');
+
+ $(document).ready(function(){
+  	$('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+      // Callback for Modal close
+    }
+  );
+  });
