fcb2fd5ae0f109fa56bc8b8eca12ea2bc7a2435b
LauraAWard <l-ward@northwestern.edu>
2018-01-07 00:20:40 -0600

    Schema and Seeds added

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,103 @@
 ### Schema
+DROP DATABASE IF EXISTS gigity_DB;
+CREATE DATABASE gigity_DB;
+USE gigity_DB;
 
-CREATE DATABASE cat_db;
-USE cat_db;
-
-CREATE TABLE cats
+CREATE TABLE users
 (
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	user_id INT NOT NULL AUTO_INCREMENT,
+	username VARCHAR(45) NOT NULL,
+	userpassword VARCHAR(45) NOT NULL,
+	PRIMARY KEY (user_id)
+);
+
+CREATE TABLE freelancers
+(
+	id INT NOT NULL AUTO_INCREMENT,
+    freelancer_id INT,
+	first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(12) NOT NULL,
+    photo VARCHAR(255) NOT NULL,
+    rate DECIMAL(6,2),
+	bio LONGTEXT,
+	FOREIGN KEY(freelancer_id) REFERENCES users(user_id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE skill_types
+(
+	type_id INT NOT NULL AUTO_INCREMENT,
+    descr VARCHAR(100) NOT NULL,
+    PRIMARY KEY (type_id)
+);
+
+
+CREATE TABLE freelancer_skills
+(
+	fls_id INT NOT NULL AUTO_INCREMENT,
+    freelancer_id INT,
+    skill_id INT,
+    skill_status BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY(freelancer_id) REFERENCES freelancers(freelancer_id),
+    FOREIGN KEY(skill_id) REFERENCES skill_types(type_id),
+    PRIMARY KEY (fls_id) 
+);
+
+
+CREATE TABLE employers
+(
+	id INT NOT NULL AUTO_INCREMENT,
+    employer_id INT,
+	first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    company VARCHAR(255),
+    FOREIGN KEY(employer_id) REFERENCES users(user_id),
 	PRIMARY KEY (id)
 );
+
+CREATE TABLE job_stats
+(
+	status_id INT NOT NULL AUTO_INCREMENT,
+    descr VARCHAR(25) NOT NULL,
+    PRIMARY KEY (status_id)
+);
+
+CREATE TABLE jobs
+(
+	job_id INT NOT NULL AUTO_INCREMENT, 
+	employer_id INT,
+    freelancer_id INT,
+    job_descr LONGTEXT,
+    job_status INT,
+    FOREIGN KEY(employer_id) REFERENCES employers(employer_id),
+	FOREIGN KEY(freelancer_id) REFERENCES freelancers(freelancer_id),
+    FOREIGN KEY(job_status) REFERENCES job_stats(status_id),
+	PRIMARY KEY (job_id)
+);
+
+CREATE TABLE reviews
+(
+	review_id INT NOT NULL AUTO_INCREMENT, 
+    reviewer_id INT,
+    reviewee_id INT,
+    rating TINYINT,
+    review LONGTEXT,
+	FOREIGN KEY(reviewer_id) REFERENCES users(user_id),
+	FOREIGN KEY(reviewee_id) REFERENCES users(user_id),
+	PRIMARY KEY (review_id)
+);
+/*
+CREATE TABLE messages
+(
+	message_id INT NOT NULL AUTO_INCREMENT, 
+	message_to INT,
+    message_from INT,
+    message LONGTEXT,
+    FOREIGN KEY(message_to) REFERENCES users(user_id),
+	FOREIGN KEY(message_from) REFERENCES users(user_id),
+	PRIMARY KEY (message_id)
+);
+*/
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,26 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+
+USE gigity_DB;
+
+INSERT INTO job_stats (descr) VALUES ('open');
+INSERT INTO job_stats (descr) VALUES ('pending offer');
+INSERT INTO job_stats (descr) VALUES ('in progress');
+INSERT INTO job_stats (descr) VALUES ('completed');
+
+INSERT INTO skill_types (descr) VALUES ('Front End Developer');
+INSERT INTO skill_types (descr) VALUES ('Back End Developer');
+INSERT INTO skill_types (descr) VALUES ('Data Scientist');
+INSERT INTO skill_types (descr) VALUES ('IT');
+
+INSERT INTO users (username, userpassword) VALUES ('JohnDoe', 'DoJo1');
+INSERT INTO users (username, userpassword) VALUES ('BossMan', 'Bossy2');
+
+INSERT INTO freelancers (freelancer_id, first_name, last_name, email, phone, photo, rate, bio) VALUES (1, 'John', 'Doe', 'john@f-society.com', '555/555-5555', 'https://static.pexels.com/photos/92129/pexels-photo-92129.jpeg', 75.00, 'Able to blend in seamlessly to any office environment');
+
+INSERT INTO employers (employer_id, first_name, last_name, email, company) VALUES (2, 'Jeff', 'Smith', 'j.smith@widgets.com', 'Widgets R Us');
+
+INSERT INTO freelancer_skills (freelancer_id, skill_id, skill_status) VALUES (1, 1, true);
+INSERT INTO freelancer_skills (freelancer_id, skill_id, skill_status) VALUES (1, 4, true);
+
+INSERT INTO jobs (employer_id, job_descr, job_status) VALUES (2, 'Create storefront for dark web sales', 1);
+
+INSERT INTO reviews (reviewer_id, reviewee_id, rating, review) VALUES (1, 2, 5, 'No direct contact, paid in bitcoin');
