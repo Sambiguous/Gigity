58609bfb478bd861c4c6035e17458bd0fe3b87e5
emfritz2 <emfritz2@gmail.com>
2018-01-08 13:33:46 -0600

    html changes

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -8,6 +8,7 @@ $(".button-collapse").sideNav();
  $('.dropdown-button').dropdown('open');
  $('.dropdown-button').dropdown('close');
 
+
  $(document).ready(function(){
   	$('.modal').modal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
@@ -16,7 +17,21 @@ $(".button-collapse").sideNav();
       outDuration: 200, // Transition out duration
       startingTop: '4%', // Starting top style attribute
       endingTop: '10%', // Ending top style attribute
-      // Callback for Modal close
     }
   );
   });
+
+ (function ($) {
+    $(function () {
+
+        //initialize all modals           
+        $('.modal').modal();
+
+        //now you can open modal from code
+        $('#modal1').modal('open');
+
+        //or by click on trigger
+        $('.trigger-modal').modal();
+
+    }); // end of document ready
+});
