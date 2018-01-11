f9ceb9e8d753a9d541f7afe02f98a39a1ba1add7
Sam <sjdefrank91@gmail.com>
2018-01-10 20:59:26 -0600

    add dummy data for front end testing

--- controllers/employerController.js
+++ controllers/employerController.js
@@ -17,6 +17,35 @@ router.get("/", function(req, res) {
     });
 });
 
+router.get('/freelancerBoard', function(req, res){
+
+    let dummyData =[
+        {
+        first_name: 'john',
+        last_name: 'Doe',
+        skills: ['skill1', 'skill3'],
+        photo: 'url',
+        rate: 5,
+        },
+        {
+        first_name: 'billy',
+        last_name: 'bob',
+        skills: ['skill1'],
+        photo: 'url',
+        rate: 3,
+        },
+        {
+        first_name: 'Jim',
+        last_name: 'Miller',
+        skills: ['skill4'],
+        photo: 'url',
+        rate: 5,
+        },
+    ]
+
+    res.render()
+}
+
 
 router.post("/signup", function(req, res) {
 
