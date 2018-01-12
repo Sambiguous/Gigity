f994860f0fffb91496745a943cac63baa0ac8f12
LauraAWard <l-ward@northwestern.edu>
2018-01-12 00:43:27 -0600

    filling in users in seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,6 +17,21 @@ INSERT INTO users (user_email, user_password, user_type) VALUES ('mary.marbles@y
 INSERT INTO users (user_email, user_password, user_type) VALUES ('I.M.Oldman@geocities.com', 'Flashback', 'F');
 INSERT INTO users (user_email, user_password, user_type) VALUES ('s-snowflake@snowblower.com', 'BringingJoy2', 'E');
 INSERT INTO users (user_email, user_password, user_type) VALUES ('George@vandelay.com', 'FestivusNow', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('kadams@gmail.com', 'TEST7', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('reginap@gmail.com', 'TEST8', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('bwyatt@cityofpawnee.com', 'TEST9', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('wtruman@yahoo.com', 'TEST10', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('cday@hotmail.com', 'sunny11', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('mrichards@gmail.com', 'newman12', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('dee@iasip.com', 'TEST13', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('justjack@gmail.com', 'TEST14', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('lknope@cityofpawnee.com', 'TEST15', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('gob@bluthsbananas.com', 'magic16', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('rachel@gmail.com', 'TEST17', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('giggity@gmail.com', 'lois4ever', 'E');
+INSERT INTO users (user_email, user_password, user_type) VALUES ('cyork@nyc.com', 'TEST19', 'E');
+
+
 
 INSERT INTO freelancers (user_id, first_name, last_name, email, photo, rate, bio) VALUES (1, 'John', 'Doe', 'john@f-society.com', 'https://static.pexels.com/photos/92129/pexels-photo-92129.jpeg', 75.00, 'Able to blend in seamlessly to any office environment');
 INSERT INTO freelancers (user_id, first_name, last_name, email, photo, rate, bio) VALUES (3, 'Mary', 'Marbles', 'mary.marbles@yahoo.com', 'https://static.pexels.com/photos/746801/pexels-photo-746801.jpeg', 90.00, 'Always looking at the big picture');
